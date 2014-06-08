70cbc15318ab7f2effc15b7074d850ea058b6f4c
Gene De Lisa <gene@rockhoppertech.com>
2014-06-08 15:07:39 -0400

    Initial Commit

---
+++ SwiftSimpleAUGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSimpleAUGraph
+//
+//  Created by Gene De Lisa on 6/8/14.
+//  Copyright (c) 2014 Gene De Lisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
